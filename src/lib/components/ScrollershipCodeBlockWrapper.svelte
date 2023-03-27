6ac63561e4145b432a61035d856425238d5725b8
Ben Schmidt <bmschmidt@gmail.com>
2023-03-27 10:28:10 -0400

    only different for Ben

--- src/lib/components/ScrollershipCodeBlockWrapper.svelte
+++ src/lib/components/ScrollershipCodeBlockWrapper.svelte
@@ -4,7 +4,7 @@
 
 	import ScrollershipCodeBlock from 'pandoc-svelte-components/custom/ScrollershipCodeBlock.svelte';
 
-	if (process.env.NODE_ENV === 'development') {
+	if (process.env.NODE_ENV === 'development' && process.env.USER === 'ben') {
 		const code = data[1].replace('https://static.nomic.ai/tiles/', 'http://localhost:8080/');
 		data[1] = code;
 	}
